887dd8006ade2f468fa0eb63dd87f9ec299f30d6
tzah.m <marciano.tz@gmail.com>
2024-11-29 16:22:23 +0200

    init commit

--- 114.py
+++ 114.py
@@ -4,8 +4,8 @@ response = requests.get('https://en.wikipedia.org/')
 if response.status_code == 200:
     print('Page loaded lllllllsuccessfully')
     print(type(response))
+    print(type(respdewronse))
     print(type(response))
-    print(type(response))
-    print(type(response))
-
+    print(type(res32234ponse))
+    # TODO asdasdasdaasdasfasfa
     print(response.text[:100])
